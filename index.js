5595487cd0a4815995cec56385e039556f6c7122
achchuthan <achch1990@gmail.com>
2021-10-08 14:53:03 +0530

    Post and User

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {sequelize,User}  = require("./models");
+const {sequelize,User,Post}  = require("./models");
 
 const app = express();
 app.use(express.json());
@@ -27,14 +27,72 @@ app.get('/users/:uuid',async (req,res)=>{
     const uuid = req.params.uuid;
     try{
         const user = await User.findOne({
-            where:{uuid}
+            where:{uuid},
+            include:['posts'],
         });
         return res.json(user);
     }catch (e) {
         console.log(e);
         return res.status(500).json({error:'Something went wrong'});
     }
-})
+});
+
+app.delete('/users/:uuid',async (req,res)=>{
+    const uuid = req.params.uuid;
+    try{
+        const user = await User.findOne({
+            where:{uuid},
+        });
+        await user.destroy();
+        return res.json({message:"User Deleted!"});
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json({error:'Something went wrong'});
+    }
+});
+app.put('/users/:uuid',async (req,res)=>{
+    const uuid = req.params.uuid;
+    const {name,email,role} = req.body;
+    try{
+        const user = await User.findOne({
+            where:{uuid},
+        });
+        user.name = name;
+        user.email = email;
+        user.role = role;
+        await user.save();
+
+        return res.json(user);
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json({error:'Something went wrong'});
+    }
+});
+
+app.post('/posts',async (req,res)=>{
+const {userUuid,body} = req.body;
+
+try{
+    const user = await User.findOne({where:{uuid:userUuid}});
+    const post = await Post.create({body,userId:user.id});
+    return res.json(post);
+}catch (e) {
+    console.log(e);
+    return res.status(500).json(e);
+}
+});
+app.get('/posts',async (req,res)=>{
+    try{
+        const posts = await Post.findAll({
+            include: ['user']
+    });
+        return res.json(posts);
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json({error:e});
+    }
+});
+
 app.listen({port:5000},async ()=>{
     console.log('Server Listening on 5000');
     await sequelize.authenticate();
--- models/user.js
+++ models/user.js
@@ -9,8 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Post}) {
       // define association here
+      this.hasMany(Post,{foreignKey:'userId',as:'posts'})
     }
     toJSON() {
       return {...this.get(),id:undefined};
@@ -23,15 +24,28 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull:false,
+      validate:{
+        notNull:{msg:'User must have a name'},
+        notEmpty:{msg:'Name must not be empty'}
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull:false,
+      validate:{
+        notNull:{msg:'User must have a email'},
+        notEmpty:{msg:'Email must not be empty'},
+        isEmail:{msg:'Must be a valid email address'}
+      }
     },
     role: {
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull:false,
+      validate:{
+        notNull:{msg:'User must have a role'},
+        notEmpty:{msg:'Role must not be empty'}
+      }
     }
   }, {
     sequelize,
